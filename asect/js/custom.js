703f1d0366c673bb0f8cca64f88f3c7754057ef6
prince954 <akprince0268@gmail.com>
2023-04-12 01:18:08 +0600

    Style : Header Serch Design

--- asect/js/custom.js
+++ asect/js/custom.js
@@ -3,4 +3,30 @@ document.addEventListener('click',function(e){
   if(e.target.classList.contains('hamburger-toggle')){
     e.target.children[0].classList.toggle('active');
   }
-}) 
+}) ;
+
+// header serch icone 
+
+const header_serch_icon = document.querySelector(".header-serch-icone a i");
+const header_serch = document.querySelector(".header-serch");
+
+header_serch_icon.addEventListener("click",()=>{
+    const isopen = header_serch.classList.toggle("displayNone");
+     header_serch_icon.classList = isopen ? 'fa-solid fa-magnifying-glass' : 'ph ph-x';
+ });
+
+// header_serch_icon.addEventListener('click',()=>{
+  
+//   const isOpen = header_serch_icon.classList.toggle("displaayNone");
+
+// })
+ 
+
+
+//  toggle.addEventListener("click", () => {
+
+//      const isopen = nav_section.classList.toggle("active");
+
+//      toggle.classList = isopen ? 'ph ph-x' : 'ph ph-equals';
+
+//  });
