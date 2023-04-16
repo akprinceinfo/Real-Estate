3304269ec2b846f3edcfc118ebe6989adeccf9a6
prince954 <akprince0268@gmail.com>
2023-04-17 00:20:23 +0600

    Js : Header card js Done

--- asect/js/custom.js
+++ asect/js/custom.js
@@ -15,6 +15,21 @@ header_serch_icon.addEventListener("click",()=>{
      header_serch_icon.classList = isopen ? 'fa-solid fa-magnifying-glass' : 'ph ph-x';
  });
 
+
+// this is card design 
+const shopping_cart_icon = document.querySelector(".shopping-cart-icon a");
+const card_icon = document.querySelector(".card-icon i");
+const card_design = document.querySelector(".card-design");
+
+shopping_cart_icon.addEventListener("click",()=>{
+  card_design.classList.add("card-design-hide");
+});
+card_icon.addEventListener("click",()=>{
+  card_design.classList.remove("card-design-hide");
+});
+
+
+
 // header_serch_icon.addEventListener('click',()=>{
   
 //   const isOpen = header_serch_icon.classList.toggle("displaayNone");
